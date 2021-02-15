074fd2c8cc6ac9f7c4853b8e7bb6b159461afc82
AconCavy <aconcavy@gmail.com>
2021-02-16 08:05:40 +0900

    Update ps1

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -6,31 +6,26 @@ $root = Get-Location
 
 $assets = @()
 
-if ($tag -like 'v*-*')
-{
+if ($tag -like 'v*-*') {
     $assets += '-p'
 }
 
 Get-ChildItem -Path $publish -Directory |
-        ForEach-Object {
-            $target = $_.Name
-            $dest = "$publish/$target"
-            $ext = ""
-            if ($_ -like '*win-*')
-            {
-                $ext = ".zip"
-                Compress-Archive -Path $dest -DestinationPath $dest$ext
-            }
-            else
-            {
-                $ext = ".tar.gz"
-                Set-Location $publish
-                tar -zcvf $target$ext "$target"
-                Set-Location $root
-            }
-            $assets += "-a $dest$ext"
-        }
-
-
+ForEach-Object {
+    $target = $_.Name
+    $dest = "$publish/$target"
+    $ext = ""
+    if ($_ -like '*win-*') {
+        $ext = ".zip"
+        Compress-Archive -Path $dest -DestinationPath $dest$ext
+    }
+    else {
+        $ext = ".tar.gz"
+        Set-Location $publish
+        tar -zcvf $target$ext "$target"
+        Set-Location $root
+    }
+    $assets += "-a $dest$ext"
+}
 
 hub release create $( $assets -join ' ' ) -m $tag $tag
