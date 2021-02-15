0c213190f72dbc29d2a69c0cbb6ff9746a4f7fd8
AconCavy <aconcavy@gmail.com>
2021-02-16 07:54:09 +0900

    Update ps1

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -1,28 +1,36 @@
 Param([string] $publish, [string] $tag)
 
+echo "publish = $publish"
+echo "tag = $tag"
 $root = Get-Location
 
 $assets = @()
-Get-ChildItem -Path $publish -Directory |
-ForEach-Object {
-    $target = $_.Name
-    $dest = "$publish/$target"
-    $ext = ""
-    if ($_ -like '*win-*') {
-        $ext = ".zip"
-        Compress-Archive -Path $dest -DestinationPath $dest$ext
-    }
-    else {
-        $ext = ".tar.gz"
-        Set-Location $publish
-        tar -zcvf $target$ext "$target"
-        Set-Location $root
-    }
-    $assets += "-a $dest$ext"
-}
 
-if ($tag -like 'v*-*') {
+if ($tag -like 'v*-*')
+{
     $assets += '-p'
 }
 
-hub release create $($assets -join ' ') $tag
+Get-ChildItem -Path $publish -Directory |
+        ForEach-Object {
+            $target = $_.Name
+            $dest = "$publish/$target"
+            $ext = ""
+            if ($_ -like '*win-*')
+            {
+                $ext = ".zip"
+                Compress-Archive -Path $dest -DestinationPath $dest$ext
+            }
+            else
+            {
+                $ext = ".tar.gz"
+                Set-Location $publish
+                tar -zcvf $target$ext "$target"
+                Set-Location $root
+            }
+            $assets += "-a $dest$ext"
+        }
+
+
+
+hub release create $( $assets -join ' ' ) -m $tag $tag
